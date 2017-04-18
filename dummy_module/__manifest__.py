f48cad0656ca18093c71dafb1abca4bf0aa4d8bb
Bharat Parmar(bhp) <bhp@odoo.com>
2017-04-18 11:42:47 +0530

    first_coomit_from_master

---
+++ dummy_module/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Module Name',
+    'version': '0.1',
+    'description': """
+    Description of module""",
+    'author': 'Bharat Parmar',
+    'website': 'https://www.odoo.com',
+    'depends': ['base'],
+    'data': [
+    ],
+    'demo': [],
+}
